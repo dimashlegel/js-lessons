ebc10816362f4649e491025886d6e6aae0c807ab
Dima Shlegel <dimashlegel@gmail.com>
2019-07-19 12:19:38 +0300

    to uppercase input separeted value

--- practic1/script.js
+++ practic1/script.js
@@ -1,17 +1,36 @@
 'use strict'
 
 let inp = document.getElementById('inp');
-let names = document.getElementsByClassName('name');
 
-inp.addEventListener('blur', getSum);
+// inp.addEventListener('blur', getSum);
 
-function getSum() {
-	let str = inp.value.split(' ');
-	console.log(str);
+// function getSum() {
 
-	for (let i = 0; i < str.length; i++) {
-		const element = str[i];
-		names[i].value = str[i];
+// 	let str = inp.value.split(' ');
+// 	let newArr = [];
+// 	for (let i = 0; i < str.length; i++) {
+// 		const element = str[i];
+// 		let newStr = element.replace(element.charAt(0), element.charAt(0).toUpperCase());
+// 		newArr.push(newStr);
+// 	}
+// 	inp.value = newArr.join(' ');
+// }
+
+// var 2
+
+inp.addEventListener('blur', bigLetter);
+
+function uswords(str) {
+	var arr = str.split(' ');
+	for (var i = 0; i < arr.length; i++) {
+		arr[i] = arr[i].split('');
+		arr[i][0] = arr[i][0].toUpperCase();
+		arr[i] = arr[i].join('');
 	}
-
+	str = arr.join(' ');
+	return str;
 }
+
+function bigLetter() {
+	inp.value = uswords(inp.value);
+}  
