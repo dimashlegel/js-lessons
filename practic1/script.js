112ecef0fc8763d1998b2b13047d7be48121541a
Dima Shlegel <dimashlegel@gmail.com>
2019-07-19 13:07:05 +0300

    return input data in other order

--- practic1/script.js
+++ practic1/script.js
@@ -1,29 +1,14 @@
 'use strict'
 
 let inp = document.getElementById('inp');
-let par = document.getElementById('par');
-console.log(par.innerHTML);
 
-inp.addEventListener('blur', getValue);
+inp.addEventListener('blur', transformData);
 
-function longestWord(str) {
-	
-	let arrWord = str.split(' ');
-	let wordL = [];
-	let rez;
+function transformData() {
+	let str = inp.value;
+	let arr = str.split('.');
 
-	for (let i = 0; i < arrWord.length; i++) {
-		const word = arrWord[i];
-		wordL.push(word.length);
-	}
-	rez = getMaxOfArray(wordL);
-	return rez;
-}
-
-function getValue() {
-	par.innerHTML = longestWord(inp.value);
-}
-
-function getMaxOfArray(numArray) {
-	return Math.max.apply(null, numArray);
+	let arr2 = arr.reverse();
+	let newStr = arr2.join('-');
+	inp.value = newStr;
 }
