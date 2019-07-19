d47d22489ad8f375e3e22f732b6ec7076f3c655f
Dima Shlegel <dimashlegel@gmail.com>
2019-07-19 11:58:48 +0300

    get separated names from one input

--- practic1/script.js
+++ practic1/script.js
@@ -1,20 +1,17 @@
 'use strict'
 
 let inp = document.getElementById('inp');
+let names = document.getElementsByClassName('name');
 
 inp.addEventListener('blur', getSum);
 
 function getSum() {
-	let str = inp.value.split(',');
+	let str = inp.value.split(' ');
 	console.log(str);
-	
-	let rez = 0;
-	
+
 	for (let i = 0; i < str.length; i++) {
 		const element = str[i];
-		rez += Number(element);
+		names[i].value = str[i];
 	}
-	let average = rez/str.length
-	console.log(average);
-	
+
 }
