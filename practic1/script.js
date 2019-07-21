3c56315c9ca25f578f6e7d8ecf774e6a98e75d9e
Dima <dima@simply-smart.com>
2019-07-21 17:45:17 +0300

    get day of week from input data

--- practic1/script.js
+++ practic1/script.js
@@ -5,8 +5,13 @@ let inp = document.getElementById('inp');
 inp.addEventListener('blur', getYear);
 
 function getYear() {
-	let date = new Date();
-	let currentY = date.getFullYear();
-	let rez = currentY - Number(inp.value);
-	console.log(rez);
+	let weak = ['Ned', 'Pon', 'Viv', 'Ser', 'Chet', 'Piat', 'Sub'];
+	let str = inp.value;
+	let arr = str.split('.');
+	let arr1 = arr.reverse();
+	var str1 = arr1.join(',');
+	let date = new Date(str1);
+	console.log(date);
+	let currentDay = date.getDay();
+	console.log(weak[currentDay]);
 }
