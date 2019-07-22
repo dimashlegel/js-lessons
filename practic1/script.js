8f7517317a43799f693788e892283d71cf35eb41
Dima <dima@simply-smart.com>
2019-07-22 16:50:59 +0300

    increase img width on click

--- practic1/script.js
+++ practic1/script.js
@@ -1,22 +1,10 @@
 'use strict'
 
-let arr = document.getElementsByTagName('a');
+let img = document.getElementById('img');
 
-window.addEventListener('DOMContentLoaded', addHref);
+img.addEventListener('click', zoomImg);
 
-function addHref() {
-	for (let i = 0; i < arr.length; i++) {
-		const element = arr[i];
-		// var 1
-
-		// if (element.getAttribute('href').includes('http://')) {
-		// 	element.innerHTML += element.getAttribute('href');
-		// }
-		
-		// var 2
-
-		if (element.getAttribute('href').indexOf('http://') == 0) {
-			element.innerHTML += element.getAttribute('href');
-		}
-	}
+function zoomImg() {
+	console.log(this.width);
+	this.width *= 2;
 }
