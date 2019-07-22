35bba73f0cd0f01a3fa41241af5fbe66c6c79cc0
Dima <dima@simply-smart.com>
2019-07-22 17:05:52 +0300

    add and remove Event Listener

--- practic1/script.js
+++ practic1/script.js
@@ -2,9 +2,31 @@
 
 let img = document.getElementById('img');
 
-img.addEventListener('click', zoomImg);
+// var 1
 
-function zoomImg() {
-	console.log(this.width);
-	this.width *= 2;
+// img.addEventListener('click', zoomImg);
+// let counter = 0;
+// function zoomImg() {
+// 	counter += 1;
+// 	if (counter % 2 == 0) {
+// 		this.width = this.width / 2;
+// 		return;
+// 	}
+// 	this.width *= 2;
+// }
+
+// var 2
+
+img.addEventListener('click', zoomInImg);
+
+function zoomInImg() {
+	this.height *= 2;
+	this.removeEventListener('click', zoomInImg);
+	this.addEventListener('click', zoomOutImg);
+}
+
+function zoomOutImg() {
+	this.height /= 2;
+	this.removeEventListener('click', zoomOutImg);
+	this.addEventListener('click', zoomInImg);
 }
