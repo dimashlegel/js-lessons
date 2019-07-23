bb62232b2ac4bbfda86cbd905f522e2ba54589de
Dima <dima@simply-smart.com>
2019-07-23 11:40:41 +0300

    select text on page

--- practic1/script.js
+++ practic1/script.js
@@ -1,25 +1,10 @@
 'use strict'
 
-let inp = document.getElementById('inp');
-let plus = document.getElementById('plus');
-let minus = document.getElementById('minus');
+let inp = document.querySelector('input');
+let par = document.querySelector('p');
 
-plus.addEventListener('click', increase);
-minus.addEventListener('click', decrease);
+document.documentElement.addEventListener('mouseup', func);
 
-function increase() {
-	let val = Number(inp.value);
-	val += 1;
-	inp.value = val;
-	minus.disabled = false;
-
-}
-
-function decrease() {
-	if (inp.value == 0) {
-		minus.disabled = true;
-	} else {
-		minus.disabled = false;
-		inp.value -= 1;
-	}
+function func(event) {
+	inp.value = window.getSelection().toString();
 }
