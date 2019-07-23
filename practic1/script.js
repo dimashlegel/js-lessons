fc4aa3622279668fc41041df895883071c121ddd
Dima <dima@simply-smart.com>
2019-07-23 17:00:35 +0300

    change colors from array with period 1s

--- practic1/script.js
+++ practic1/script.js
@@ -1,42 +1,15 @@
 'use strict'
 
-// var 1
+let par = document.querySelector('#par');
+let arr = ['red', 'green', 'blue'];
+let count = 0;
+// par.style.color = arr[0];
+window.setInterval(changeColor, 1000);
 
-// let inp = document.querySelector('#inp');
-// let par = document.querySelector('#par');
-// let timer;
-// let curr;
-// inp.addEventListener('blur', startTimer);
-// function startTimer() {
-// 	curr = inp.value;
-// 	timer = setInterval(decreaseFunc, 1000);
-// }
-// function decreaseFunc() {
-// 	par.innerHTML = curr - 1;
-// 	console.log(par.innerHTML);
-// 	curr -= 1;
-// 	console.log(curr);
-// 	if (curr <= 0) {
-// 		clearInterval(timer);
-// 	}
-// }
-
-// var 2
-
-var input = document.getElementById('task');
-function func() {
-	window.funcId = window.setInterval(go, 500);
- }
-
-function go() {
-	if (parseInt(input.value) > 0) {
-		input.value = parseInt(input.value) -1;
+function changeColor() {
+	par.style.color = arr[count];
+	count++;
+	if (count == arr.length) {
+		count = 0;
 	}
- 	if(parseInt(input.value) == 0) {
-		stop();
- 	}
- }
-
-function stop() {
-	window.clearInterval(window.funcId);
- } 
+}
