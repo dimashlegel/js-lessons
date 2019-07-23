e37feb3516b524ac49728028009a4b1621f7c7f3
Dima <dima@simply-smart.com>
2019-07-23 15:17:09 +0300

    count down (obrat schet)

--- practic1/script.js
+++ practic1/script.js
@@ -1,19 +1,42 @@
 'use strict'
 
-let inp = document.querySelector('#inp');
-let inpNum = document.querySelector('#inpNum');
-let inpSymb = document.querySelector('#inpSymb');
+// var 1
 
-let button = document.querySelector('button');
+// let inp = document.querySelector('#inp');
+// let par = document.querySelector('#par');
+// let timer;
+// let curr;
+// inp.addEventListener('blur', startTimer);
+// function startTimer() {
+// 	curr = inp.value;
+// 	timer = setInterval(decreaseFunc, 1000);
+// }
+// function decreaseFunc() {
+// 	par.innerHTML = curr - 1;
+// 	console.log(par.innerHTML);
+// 	curr -= 1;
+// 	console.log(curr);
+// 	if (curr <= 0) {
+// 		clearInterval(timer);
+// 	}
+// }
 
-button.addEventListener('click', createString);
+// var 2
 
-function createString() {
-	let str = '';
-	let symbols = inpSymb.value;
-	for (let i = 0; i < inpNum.value; i++) {
-		let symbol = symbols.charAt(Math.floor(Math.random() * symbols.length));
-		str += symbol;
+var input = document.getElementById('task');
+function func() {
+	window.funcId = window.setInterval(go, 500);
+ }
+
+function go() {
+	if (parseInt(input.value) > 0) {
+		input.value = parseInt(input.value) -1;
 	}
-	inp.value = str;
-}
+ 	if(parseInt(input.value) == 0) {
+		stop();
+ 	}
+ }
+
+function stop() {
+	window.clearInterval(window.funcId);
+ } 
