8fdcd6af7c25af2521dfaba49dca0ba56cba5ccd
Dima Shlegel <dimashlegel@gmail.com>
2019-08-24 20:22:57 +0300

    editing text on click inside table td

--- practic1/task-52/script.js
+++ practic1/task-52/script.js
@@ -1,35 +1,20 @@
 'use strict'
 
-let tdArr = document.querySelectorAll('#table tr td');
-let btn = document.querySelector('#btn');
-let inp = document.querySelector('#inp');
-let reset = document.querySelector('#reset');
+window.onload = function() {
+	var tdelements = document.querySelectorAll('td');
 
-tdArr.forEach((elem) => {
-	elem.addEventListener('click', addActive);
-})
-
-function addActive() {
-	this.classList.add('active');
-}
-
-btn.addEventListener('click', getSum);
-
-function getSum() {
-	let activeTds = document.querySelectorAll('#table tr td.active');
-	let sum = 0;
-	activeTds.forEach((elem) => {
-		sum += +elem.innerText;
-	});
-	inp.value = sum;
-}
-
-reset.addEventListener('click', resetActive);
-
-function resetActive() {
-	let activeTds = document.querySelectorAll('#table tr td.active');
-	activeTds.forEach((elem) => {
-		elem.classList.remove('active');
-	})
-	inp.value = '';
+	for (var i = 0; i < tdelements.length; i++) {
+		tdelements[i].addEventListener('click', function() {
+			var val = this.innerHTML;
+			this.style.display = "none";
+			var inp = document.createElement('input');
+			inp.value = val;
+			inp.addEventListener('change', function() {
+				this.previousElementSibling.innerHTML = this.value;
+				this.previousElementSibling.style.display = "";
+				this.parentNode.removeChild(this);
+			});
+			this.parentNode.insertBefore(inp, this.nextSibling);
+		});
+	}
 }
