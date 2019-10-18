895b4daa736b621fee8669b931f04d8186f8f6d9
Dima Shlegel <dimashlegel@gmail.com>
2019-10-18 13:38:17 +0300

    continue work on calendar

--- practic2/calendar/script.js
+++ practic2/calendar/script.js
@@ -13,47 +13,47 @@ let weekDay = data.getDay();
 let day = data.getDate();
 
 let monthes = ['Januar', 'Februar', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+let week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-// var optionsMonth = {
-// 	month: 'long',
-// };
-// var optionsDay = {
-// 	weekday: 'long',
-// };
+let monthIndex = month; // counter;
+let yearIndex = year; // counter;
 
-
-// console.log('data:', data.getDate() + ' ' + new Intl.DateTimeFormat('en-US', optionsMonth).format(data) + ' ' + data.getFullYear());
-// console.log('day:', new Intl.DateTimeFormat('en-US', optionsDay).format(data));
-
-function getlastDayOfMonth(data) {
-	let lastDayOfMonth = new Date(data.getFullYear(), data.getMonth() + 1, 0);
+function getlastDayOfMonth(data, monthIndex) {
+	let lastDayOfMonth = new Date(year, monthIndex + 1, 0);
 	// console.log('last day of current month:', lastDayOfMonth.getDate());
 	return lastDayOfMonth.getDate();
 }
-// console.log(getlastDayOfMonth(data)); // show last day of month;
+// console.log(getlastDayOfMonth(data, 9)); // show last day of month;
 
-function getDays(from, to, parent, child) {
+function getDays(from, to, parent, child, currentMonth) { // get all days in month
 	let wrapper = document.createElement(parent);
 	for (let i = from; i <= to; i++) {
 		let item = document.createElement(child);
 		item.innerHTML = i;
-		markCurrentDay(item);
-		markWeekandDay(item, i);
+		markCurrentDay(item, currentMonth);
+		markWeekandDay(item, i, currentMonth);
 		wrapper.appendChild(item);
 	}
 	return wrapper;
 }
 
-calendar.appendChild(getDays(1, getlastDayOfMonth(data), 'ul', 'li'));
+function createCalendar(month) {
+	calendar.innerHTML = "";
+	return calendar.appendChild(getDays(1, getlastDayOfMonth(data, month), 'ul', 'li', month));
+}
 
-function markCurrentDay(elem) {
-	if (elem.innerHTML == day) {
+createCalendar(month);
+getFirstDayOfCurrMonth(month);
+howManyDaysToMonday(month);
+
+function markCurrentDay(elem, currentMonth) { // show green current day
+	if (elem.innerHTML == day && month == currentMonth) {
 		elem.style.color = 'green';
 	}
 }
 
-function markWeekandDay(item, elem) {
-	let weekandDay = new Date(year, month, elem);
+function markWeekandDay(item, elem, currentMonth) { // show red weekend day
+	let weekandDay = new Date(year, currentMonth, elem);
 	if (weekandDay.getDay() == 0 || weekandDay.getDay() == 6) {
 		item.style.color = 'red';
 	}
@@ -65,16 +65,20 @@ function showCurrentYearMonth(currYear, currMonth) {
 
 display.innerHTML = showCurrentYearMonth(year, month);
 
-let monthIndex = month; // counter;
-let yearIndex = year; // counter;
 nextMonth.addEventListener('click', function() {
 	if (monthIndex >= 11) {
 		monthIndex = 0;
 		yearIndex += 1;
 		showNextPrevYearMonth(yearIndex, monthIndex);
+		createCalendar(monthIndex);
+		getFirstDayOfCurrMonth(monthIndex);
+		howManyDaysToMonday(monthIndex);
 	} else {
 		monthIndex += 1;
 		showNextPrevYearMonth(yearIndex, monthIndex);
+		createCalendar(monthIndex);
+		getFirstDayOfCurrMonth(monthIndex);
+		howManyDaysToMonday(monthIndex);
 	}
 });
 
@@ -83,12 +87,54 @@ prevMonth.addEventListener('click', function() {
 		monthIndex = 11;
 		yearIndex -= 1;
 		showNextPrevYearMonth(yearIndex, monthIndex);
+		createCalendar(monthIndex);
+		getFirstDayOfCurrMonth(monthIndex);
+		howManyDaysToMonday(monthIndex);
 	} else {
 		monthIndex -= 1;
 		showNextPrevYearMonth(yearIndex, monthIndex);
+		createCalendar(monthIndex);
+		getFirstDayOfCurrMonth(monthIndex);
+		howManyDaysToMonday(monthIndex);
 	}
 });
 
 function showNextPrevYearMonth(currentYear, currentMonth) {
 	display.innerHTML = showCurrentYearMonth(currentYear, currentMonth);
 }
+
+function getFirstDayOfCurrMonth(currentMonth) {
+	let FirstDayOfMonth = new Date(year, currentMonth, 1);
+	console.log('First Day of current Month: ' + week[FirstDayOfMonth.getDay()]);
+	return week[FirstDayOfMonth.getDay()];
+}
+
+function howManyDaysToMonday(currentMonth) {
+	let FirstDayOfMonth = new Date(year, currentMonth, 1);
+	let firstDay = FirstDayOfMonth.getDay();
+	let dayName = week[firstDay];
+	let counter = 0;
+
+	for (let i = 0; i < 7; i++) {
+		if (dayName === 'Monday') {
+			console.log('Days from Monday: ' + counter);
+			return counter;
+		} else {
+			counter += 1;
+			firstDay -= 1;
+			if (firstDay < 0) {
+				firstDay = 6;
+			}
+			dayName = week[firstDay];
+		}
+	}
+}
+
+// var optionsMonth = {
+// 	month: 'long',
+// };
+// var optionsDay = {
+// 	weekday: 'long',
+// };
+// console.log('data:', data.getDate() + ' ' + new Intl.DateTimeFormat('en-US', optionsMonth).format(data) + ' ' + data.getFullYear());
+// console.log('day:', new Intl.DateTimeFormat('en-US', optionsDay).format(data));
