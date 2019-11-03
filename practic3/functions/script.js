1d97da402a8996b873a428ab9f9a2a334df3f822
Dima Shlegel <dimashlegel@gmail.com>
2019-11-03 22:48:16 +0200

    add functions 1.deleteElement, 2.getDiffElemInArrays, 3.randomElementInArray

---
+++ practic3/functions/script.js
@@ -0,0 +1,72 @@
+'use strict'
+
+// 1. Сделайте функцию detElem, которая параметром будет принимать массив и какое значение и возвращать массив, в котором все элементы с таким значением будут удалены.
+// delete elems from array
+
+function detElem(array, num) {
+	for (let i = 0; i < array.length; i++) {
+		const element = array[i];
+		if (element === num) {
+			array.splice(i, 1);
+		}
+	}
+	return array;
+}
+detElem([1, 2, 3, 4, 5, 6, 7, 8, 1, 2], 2);
+//
+//
+//
+// 2. Сделайте функцию getDiff, которая параметром будет принимать два массива и возвращать массив элементов, которые не присутствуют одновременно в обоих массивах.
+// 1 method
+function getDiff(arr1, arr2) {
+	let arrRez = [];
+	for (let i = 0; i < arr1.length; i++) {
+		const element = arr1[i];
+		if (arr2.indexOf(element) === -1 && arrRez.indexOf(element) === -1) {
+			arrRez.push(element);
+		}
+	}
+	for (let i = 0; i < arr2.length; i++) {
+		const element = arr2[i];
+		if (arr1.indexOf(element) === -1 && arrRez.indexOf(element) === -1) {
+			arrRez.push(element);
+		}
+	}
+	return arrRez;
+}
+getDiff([1, 2, 3], [1, 4, 5]);
+//
+//
+// 2 method
+// function getDiff(arr1, arr2) {
+// 	arrRez = arr1.concat(arr2);
+// 	for (var i = 0; i < arr1.length; i++) {
+// 		for (var j = 0; j < arr2.length; j++) {
+// 			if (arr1[i] == arr2[j]) {
+// 				arrRez = detElem(arrRez, arr1[i]);
+// 			}
+// 		}
+// 	}
+// 	return arrRez;
+// }
+// getDiff(arr1, arr2);
+//
+//
+//
+// 3. Сделайте функцию arrayRand, которая параметром будет принимать массив и возвращать случайный элемент из этого массива.
+function arrayRand(arr) {
+	let arrLength = arr.length;
+	let random = Math.floor(Math.random() * Math.floor(arrLength));
+	return arr[random];
+}
+arrayRand([1, 2, 3]);
+//
+//
+//
+//
+// 4. Сделайте функцию uniq, которая будет оставлять в массиве только уникальные элементы (то есть будет удалять дубли)
+
+function uniq(arr) {
+
+}
+uniq([1, 2, 3, 1, 2]);
