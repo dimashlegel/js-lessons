4e15229c3a816191ace2ffc9f720d1b2d5925861
Dima <dima@simply-smart.com>
2020-04-11 22:12:03 +0300

    static methods and properties

--- OOP/script.js
+++ OOP/script.js
@@ -1,5 +1,79 @@
 'use strict'
+// learnjs
+//класс – это разновидность функции.
 
+// Class Expression (по аналогии с Function Expression):
+let User1 = class {
+	sayHi() {
+		alert("Привет");
+	}
+};
+// task extend Class
+
+class Clock {
+  constructor({ template }) {
+    this.template = template;
+  }
+  render() {
+    let date = new Date();
+    let hours = date.getHours();
+    if (hours < 10) hours = '0' + hours;
+    let mins = date.getMinutes();
+    if (mins < 10) mins = '0' + mins;
+    let secs = date.getSeconds();
+    if (secs < 10) secs = '0' + secs;
+    let output = this.template
+      .replace('h', hours)
+      .replace('m', mins)
+      .replace('s', secs);
+    console.log(output);
+  }
+  stop() {
+    clearInterval(this.timer);
+  }
+  start() {
+    this.render();
+    this.timer = setInterval(() => this.render(), 1000);
+  }
+}
+
+// Создайте новый класс ExtendedClock, который будет наследоваться от Clock и добавьте параметр precision – количество миллисекунд между «тиками». Установите значение в 1000 (1 секунда) по умолчанию.
+class ExtendedClock extends Clock {
+  constructor(options) {
+    super(options);
+    let {precision=1000} = options;
+    this.precision = precision;
+  }
+
+  start() {
+    this.render();
+    this.timer = setInterval(() => this.render(), this.precision);
+  }
+};
+// 
+let lowResolutionClock = new ExtendedClock({
+	template: 'h:m:s',
+	precision: 1000,
+});
+lowResolutionClock.start();
+
+//
+//
+// Static method 
+class User2 {
+  static staticMethod() { // Мы также можем присвоить метод самой функции-классу, а не её "prototype". Такие методы называются статическими.
+    alert(this === User2);
+  }
+}
+User2.staticMethod(); // true
+
+// Static property 
+class Article {
+  static publisher = "Илья Кантор";
+}
+alert( Article.publisher ); // Илья Кантор
+//
+//
 class User {
 	constructor(name, surname) {
 		this._name = name, // privat property
