1570288e18e3fdf3cc231d6bfbd32254eaf2dd5e
Dima <dima@simply-smart.com>
2020-04-16 23:47:16 +0300

    __proto__ methods

--- OOP/prototype/script.js
+++ OOP/prototype/script.js
@@ -56,7 +56,61 @@ Function.prototype.defer2 = function(time) {
 }
 
 function f(a, b) {
-	alert(a + b);
+	// alert(a + b);
 }
 
 f.defer2(1000)(1, 2); // выведет 3 через 1 секунду.
+
+//
+//
+// Методы прототипов, объекты без свойства __proto__
+
+// Современные же методы это:
+
+// Object.create(proto, [descriptors]) – создаёт пустой объект со свойством [[Prototype]], указанным как proto, и необязательными дескрипторами свойств descriptors.
+// Object.getPrototypeOf(obj) – возвращает свойство [[Prototype]] объекта obj.
+// Object.setPrototypeOf(obj, proto) – устанавливает свойство [[Prototype]] объекта obj как proto.
+// Эти методы нужно использовать вместо __proto__.
+
+let animal2 = {
+	eats: true
+};
+// создаём новый объект с прототипом animal
+let rabbit2 = Object.create(animal2);
+// alert(rabbit2.eats); // true
+// alert(Object.getPrototypeOf(rabbit2) === animal2); // получаем прототип объекта rabbit
+Object.setPrototypeOf(rabbit2, {}); // заменяем прототип объекта rabbit на {}
+
+// Мы также можем использовать Object.create для «продвинутого» клонирования объекта, более мощного, чем копирование свойств в цикле for..in:
+// клон obj c тем же прототипом (с поверхностным копированием свойств)
+let clone = Object.create(Object.getPrototypeOf(obj), Object.getOwnPropertyDescriptors(obj));
+
+
+//
+//
+
+// 1.1 Добавьте toString в словарь
+
+let dictionary = Object.create(null, {
+	toString: {
+		value() {
+			return Object.keys(this).join();
+		}
+	}
+});
+
+// ваш код, который добавляет метод dictionary.toString
+
+dictionary.toString;
+
+// добавляем немного данных
+dictionary.apple = "Apple";
+dictionary.__proto__ = "test"; // здесь __proto__ -- это обычный ключ
+
+// только apple и __proto__ выведены в цикле
+for (let key in dictionary) {
+	alert(key); // "apple", затем "__proto__"
+}
+
+// ваш метод toString в действии
+alert(dictionary); // "apple,__proto__"
