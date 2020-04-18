f55b8b883e71b786edea9708aedff125da75775c
Dima <dima@simply-smart.com>
2020-04-18 13:03:54 +0300

    callback

---
+++ promises_async_await/callback/script.js
@@ -0,0 +1,33 @@
+'use strict'
+
+// callback - функция, которая будет вызвана по завершению асинхронного действия.
+
+function showCircle(cx, cy, radius, callback) {
+
+	let div = document.createElement('div');
+	div.style.width = 0;
+	div.style.height = 0;
+	div.style.left = cx + 'px';
+	div.style.top = cy + 'px';
+	div.className = 'circle';
+	document.body.append(div);
+
+	setTimeout(() => {
+		div.style.width = radius * 2 + 'px';
+		div.style.height = radius * 2 + 'px';
+
+		div.addEventListener('transitionend', function handler() {
+			div.removeEventListener('transitionend', handler);
+			callback(div);
+		})
+	}, 0);
+}
+
+
+let btn = document.getElementById('btn');
+btn.addEventListener('click', function() {
+	showCircle(150, 150, 102, elem => {
+		elem.classList.add('message-ball');
+		elem.append('Hello');
+	});
+})
